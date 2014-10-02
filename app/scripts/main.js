28620f90597d7383f6bd39fbbe93c428b7146823
Chelsea Franz <chelseafranz3@gmail.com>
2014-10-02 16:36:25 -0400

    it works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,15 @@
 console.log('The Iron Yard Rocks');
 
-$(".unClick").click(function(){
-  $( this ).toggleClass("click");
+//  $(".unClick").click(function(){
+// $( this ).toggleClass("click");
+// });
+
+
+// $('section').click(function(){
+//   $('section').removeClass('open')
+//   $( this ).addClass('open');
+// })
+$('section').click(function(){
+  $('section').removeClass('one')
+  $( this ).addClass('one');
 });

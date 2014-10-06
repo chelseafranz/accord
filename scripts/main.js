6a8396934153c1e2e64707715f1c7c99f910a9e1
Chelsea Franz <chelseafranz3@gmail.com>
2014-10-06 11:12:12 -0400

    monday morning

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
 console.log('The Iron Yard Rocks');
+
+//  $(".unClick").click(function(){
+// $( this ).toggleClass("click");
+// });
+
+
+// $('section').click(function(){
+//   $('section').removeClass('open')
+//   $( this ).addClass('open');
+// })
+
+
+$('section').click(function(){
+  $('section').removeClass('one')
+  $( this ).addClass('one');
+
+});
+
+
+
+$('button').click(function(){
+  alert('Ha ha! Made you click!');
+  prompt('How are you doing today?');
+  prompt("Don't lie to me. How are you really doing?");
+  alert('Why do you insist on lying to me? Huh? All i do is love you and all you do is lie.');
+  alert("Fine. Have it your way. I'll leave you alone because CLEARLY that's how you want it");
+  prompt('Please...give me one more chance?');
+  alert('Whatever. I hate you.');
+  }
+);
